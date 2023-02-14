717e9fb76e80a1399901637a35b93d93f68bee43
Leewell-Muchiri <leewellmacharia10@gmail.com>
2023-02-14 09:42:54 +0300

    Completed lab

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,32 @@
 class Triangle
-  # write code here
+
+  attr_reader :a, :b ,:c
+
+  def initialize(a,b,c)
+    @a = a
+    @b = b
+    @c = c
+    identify_valid_triangle
+  end
+
+  def kind
+    if a == b && b == c
+      :equilateral
+    elsif a == b || b == c || a == c
+      :isosceles
+    else 
+      :scalene
+    end
+  end
+
+  private
+  def identify_valid_triangle
+    raise TriangleError, "Sides must be greater than 0" if [a, b, c].min <= 0
+    raise TriangleError, "This is not a valid triangle" if a >= b + c || b >= a + c || c >= a + b
+  end
+
+  class TriangleError < StandardError
+  end
 end
+
+
